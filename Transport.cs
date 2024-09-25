42ceec3fadb869cc1080f9df7918a1d21eac86d5
ArlekinN <stepantan02@mail.ru>
2024-09-26 02:37:51 +0300

    class AirTransport

--- Transport.cs
+++ Transport.cs
@@ -7,18 +7,31 @@
     }
     abstract class Transport
     {
-        public string name { get;  set; }
-        public TypeTS typeTS { get;  set; }
+        internal string name { get;  private protected set; }
+        internal TypeTS typeTS { get; private protected set; }
         private protected double startSpeed { get; set; }
-        public double place { get; set; }
-        public Transport(string name, TypeTS typeTs, double startSpeed)
+        internal double place { get; set; }
+        private protected Transport(string name, TypeTS typeTs, double startSpeed)
         {
             this.name = name;
             this.typeTS = typeTs;
             this.place = 0;
             this.startSpeed = startSpeed;
         }
-        public abstract void move(double currentTime);
-        private protected abstract void changeSpeed();
+        internal abstract void move(double currentTime);
+    }
+
+    class AirTransport : Transport
+    {
+        private protected double acceleratinoFactor { get; set; }
+        private protected AirTransport(string name, double acceleratinoFactor, double startSpeed) : base(name, TypeTS.air, startSpeed)
+        {
+            this.acceleratinoFactor = acceleratinoFactor;
+        }
+        // s = v0*t + a*t^2/2
+        internal override void move(double currentTime)
+        {
+            place = startSpeed * currentTime + acceleratinoFactor * Math.Pow(currentTime, 2) / 2;
+        }
     }
 }
