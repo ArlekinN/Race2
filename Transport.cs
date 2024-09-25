d987b16e7c1014e5e1904934a58d0b26136457c9
ArlekinN <stepantan02@mail.ru>
2024-09-26 02:52:42 +0300

    class GroundTransport

--- Transport.cs
+++ Transport.cs
@@ -34,4 +34,41 @@
             place = startSpeed * currentTime + acceleratinoFactor * Math.Pow(currentTime, 2) / 2;
         }
     }
+
+    class GroundTransport : Transport
+    {
+        private double timeBeforeRest;
+        private double currentTimeBeforeRest;
+        private double timeRest;
+        private double currentTimeRest;
+        private protected double currentSpeed;
+        private protected GroundTransport(string name, double timeBeforeRest, double timeRest, double startSpeed) : base(name, TypeTS.ground, startSpeed)
+        {
+            this.timeBeforeRest = timeBeforeRest;
+            this.currentTimeBeforeRest = timeBeforeRest;
+            this.timeRest = timeRest;
+            this.currentTimeRest = timeRest;
+            this.currentSpeed = startSpeed;
+        }
+
+        internal override void move(double currentTime)
+        {
+            if (currentTimeBeforeRest > 0)
+            {
+                currentTimeBeforeRest--;
+                place += startSpeed;
+            }
+            else if (currentTimeRest > 0)
+            {
+                currentTimeRest--;
+            }
+            else
+            {
+                currentTimeRest = timeRest;
+                currentTimeBeforeRest = timeBeforeRest;
+                currentSpeed = startSpeed;
+            }
+
+        }
+    }
 }
