2ab64aa225e0b9013393c06b7179473014dbb965
ArlekinN <stepantan02@mail.ru>
2024-09-26 02:29:07 +0300

    abstract class Transport

---
+++ Transport.cs
@@ -0,0 +1,24 @@
+﻿namespace Transports
+{
+    public enum TypeTS
+    {
+        air,
+        ground
+    }
+    abstract class Transport
+    {
+        public string name { get;  set; }
+        public TypeTS typeTS { get;  set; }
+        private protected double startSpeed { get; set; }
+        public double place { get; set; }
+        public Transport(string name, TypeTS typeTs, double startSpeed)
+        {
+            this.name = name;
+            this.typeTS = typeTs;
+            this.place = 0;
+            this.startSpeed = startSpeed;
+        }
+        public abstract void move(double currentTime);
+        private protected abstract void changeSpeed();
+    }
+}
