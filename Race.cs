7d42291e35dbd1a5ba97294820e3d4f0ddbee671
ArlekinN <stepantan02@mail.ru>
2024-09-26 04:35:00 +0300

    function start

--- Race.cs
+++ Race.cs
@@ -16,11 +16,13 @@ namespace GameRace
         private Dictionary<int, Transport> transports = new Dictionary<int, Transport>();
         // индекс ТС - местоположение ТС
         private Dictionary<int, double> place = new Dictionary<int, double>();
+        // индекс ТС - номер, под которым ТС финишировало 
+        private Dictionary<int, int> placeRacer = new Dictionary<int, int>();
         public Race(double distance, TypeRace typeRace)
         {
             this.distance = distance;
             this.typeRace = typeRace;
-            time = 0;
+            time = 1;
         }
         private void checkTypeTs(Transport ts)
         {
@@ -39,6 +41,7 @@ namespace GameRace
                 checkTypeTs(ts);
                 transports.Add(transports.Count, ts);
                 place.Add(place.Count, 0.0);
+                placeRacer.Add(placeRacer.Count, 0);
             }
             catch (Exception)
             {
@@ -47,7 +50,28 @@ namespace GameRace
         }
         public void Start()
         {
-          
+            Transport ts;
+            int placeResult = 1;
+            Console.WriteLine("!!!!!Старт гонки!!!!!");
+            while (placeResult != transports.Count + 1)
+            {
+                foreach (var TSslot in transports)
+                {
+                    if (placeRacer[TSslot.Key] != 0) continue;
+                    ts = TSslot.Value;
+                    ts.move(time);
+                    place[TSslot.Key] = ts.place;
+                    // достиг финиша
+                    if (place[TSslot.Key] >= distance)
+                    {
+                        Console.WriteLine($"{placeResult} место - {ts.name}, время финиша - {time}");
+                        placeRacer[TSslot.Key] = placeResult;
+                        placeResult++;
+                    }
+                }
+                time++;
+            }
+            Console.WriteLine("Конец гонки");
         }
     }
 }
